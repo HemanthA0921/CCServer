efced3a8f89777854903b0ba61888aaf2fb9de2a
HemanthA0921 <97177861+HemanthA0921@users.noreply.github.com>
2023-10-04 15:59:51 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+// Sample data
+const sampleData = [
+  { id: 1, name: "Item 1" },
+  { id: 2, name: "Item 2" },
+  { id: 3, name: "Item 3" },
+];
+
+// Endpoint to get sample data
+app.get("/api/data", (req, res) => {
+  res.json(sampleData);
+  
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
